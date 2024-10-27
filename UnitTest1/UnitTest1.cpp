1f0fc9b73e75914d906585c257c562539dcb0502
8Alex38 <se64119@gmail.com>
2024-10-27 13:22:21 +0200

    commit A

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,44 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../АП-6.1/АП-6.1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestAP61
+{
+    TEST_CLASS(AP61Tests)
+    {
+    public:
+
+        TEST_METHOD(TestGenerateArray)
+        {
+            const int size = 5;
+            int arr[size];
+            int minValue = -10, maxValue = 10;
+
+            generateArray(arr, size, minValue, maxValue);
+
+            for (int i = 0; i < size; i++)
+            {
+                Assert::IsTrue(arr[i] >= minValue && arr[i] <= maxValue, L"Element is out of range");
+            }
+        }
+
+        TEST_METHOD(TestCountAndSum)
+        {
+            int arr[] = { 1, 2, -3, 4, 5 };
+            int size = sizeof(arr) / sizeof(arr[0]);
+
+            int expectedCount = 2;
+            int expectedSum = 6;
+
+            int actualCount, actualSum;
+
+            countAndSum(arr, size, actualCount, actualSum);
+
+            Assert::IsTrue(actualCount == expectedCount, L"Incorrect count");
+            Assert::IsTrue(actualSum == expectedSum, L"Incorrect sum");
+        }
+
+    };
+}
---
+++ АП-6.1/АП-6.1.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+void generateArray(int arr[], int size, int minValue, int maxValue) {
+    for (int i = 0; i < size; i++) {
+        arr[i] = minValue + rand() % (maxValue - minValue + 1);
+    }
+}
+
+void printArray(const int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        cout << setw(4) << arr[i];
+    }
+    cout << endl;
+}
+
+void countAndSum(const int arr[], int size, int& count, int& sum) {
+    count = 0;
+    sum = 0;
+    for (int i = 0; i < size; i++) {
+        if (arr[i] > 0 && arr[i] % 2 != 0) {
+            count++;
+            sum += arr[i];
+        }
+    }
+}
+
+void replaceWithZero(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        if (arr[i] > 0 && arr[i] % 2 != 0) {
+            arr[i] = 0;
+        }
+    }
+}
+
+int main() {
+    srand(time(0));
+    const int size = 23;
+    int arr[size];
+
+    generateArray(arr, size, -25, 80);
+
+    cout << " Start:        ";
+    printArray(arr, size);
+
+    int count, sum;
+    countAndSum(arr, size, count, sum);
+    cout << " Satisfaction:   " << count << endl;
+    cout << " Sum:            " << sum << endl;
+
+    replaceWithZero(arr, size);
+
+    cout << " Modification: ";
+    printArray(arr, size);
+
+    return 0;
+}
